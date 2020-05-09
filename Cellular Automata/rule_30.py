77bef5abeaa99ccfe4728901ec560374ef731c96
Adar Kahiri <adar.kahiri4@gmail.com>
2020-05-09 12:40:31 -0400

    defined rule 30

--- Cellular Automata/rule_30.py
+++ Cellular Automata/rule_30.py
@@ -9,7 +9,27 @@ cols = 60
 a =  np.zeros((rows, cols), int)
 a[0][round(cols/2)] = 1
 
-#Defining the function that will be used
+#Defining rule 30
+def rule_30(matrix, cell_row, cell_col):
+    if matrix[cell_row, cell_col] == 1 and matrix[cell_row -1, cell_col] == 1 and matrix[cell_row + 1, cell_col] == 1:
+        matrix[cell_row, cell_col] = 0
+    elif matrix[cell_row, cell_col] == 1 and matrix[cell_row -1, cell_col] == 1 and matrix[cell_row + 1, cell_col] == 0:
+        matrix[cell_row, cell_col] = 0
+    elif matrix[cell_row, cell_col] == 0 and matrix[cell_row -1, cell_col] == 1 and matrix[cell_row + 1, cell_col] == 1:
+        matrix[cell_row, cell_col] = 0
+    elif matrix[cell_row, cell_col] == 0 and matrix[cell_row -1, cell_col] == 1 and matrix[cell_row + 1, cell_col] == 0:
+        matrix[cell_row, cell_col] = 1
+    elif matrix[cell_row, cell_col] == 1 and matrix[cell_row -1, cell_col] == 0 and matrix[cell_row + 1, cell_col] == 1:
+        matrix[cell_row, cell_col] = 1
+    elif matrix[cell_row, cell_col] == 1 and matrix[cell_row -1, cell_col] == 0 and matrix[cell_row + 1, cell_col] == 0:
+        matrix[cell_row, cell_col] = 1
+    elif matrix[cell_row, cell_col] == 0 and matrix[cell_row -1, cell_col] == 0 and matrix[cell_row + 1, cell_col] == 1:
+        matrix[cell_row, cell_col] = 1
+    if matrix[cell_row, cell_col] == 0 and matrix[cell_row -1, cell_col] == 0 and matrix[cell_row + 1, cell_col] == 0:
+        matrix[cell_row, cell_col] = 0
+
+
+
 #a = np.random.random((60, 60))
 plt.figure(num=None, figsize=(10, 10), dpi=80, facecolor='w', edgecolor='k')
 plt.imshow(a)
